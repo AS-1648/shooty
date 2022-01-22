2ad883af187003bcbdb64488a1fdbab3bef17938
Cornered Sky <39480801+AS-1648@users.noreply.github.com>
2022-01-22 15:02:32 -0600

    Create shooty.lua
    when the code's only existed in a tab in vscode for the entirety of its existence and you get sick of tempting fate like that

---
+++ shooty.lua
@@ -0,0 +1,366 @@
+
+-- title:  game title
+-- author: Abiko
+-- desc:   short description
+-- script: lua
+
+--system vars
+t=0
+poke(0x03FF8, 1)-- set border colour to 1 by poking vram
+debug=true --show dev barf?
+
+--control aliases
+PAD_UP=0
+PAD_DOWN=1
+PAD_LEFT=2
+PAD_RIGHT=3
+BUTTON_A=4
+BUTTON_B=5
+BUTTON_X=6
+BUTTON_Y=7
+--colour aliases
+BLACK=0
+PURPLE=1
+RED=2
+ORANGE=3
+YELLOW=4
+LIGHT_GREEN=5
+GREEN=6
+DARK_GREEN=7
+DARK_BLUE=8
+BLUE=9
+LIGHT_BLUE=10
+CYAN=11
+WHITE=12
+LIGHT_GREY=13
+GREY=14
+DARK_GREY=15
+--sprite aliases
+SPR_PLAYER_UP=256
+SPR_PLAYER_DOWN=257
+SPR_PLAYER_LEFT=258
+SPR_PLAYER_RIGHT=259
+SPR_MONSTER=260
+SPR_AMMO_BOX=272
+--sound aliases
+GUNFIRE=0
+CLICK=1
+LOW_AMMO=2 --note: E-6
+--facing aliases
+UP=0
+DOWN=1
+LEFT=2
+RIGHT=3
+--posit aliases
+BRAZIL=-255
+
+--muzzle offsets
+MUZZLE_UP_X=4
+MUZZLE_UP_Y=0
+MUZZLE_DOWN_X=4
+MUZZLE_DOWN_Y=8
+MUZZLE_LEFT_X=0
+MUZZLE_LEFT_Y=4
+MUZZLE_RIGHT_X=8
+MUZZLE_RIGHT_Y=4
+
+
+player={--initial player stats
+ aimLock=false,
+ facing=0,
+ ammo=500,
+ kills=0,
+ posit={
+  x=120,
+  y=68
+ }
+}
+----
+
+--not yet initialised
+bullet={}
+monster={}
+score=0
+hasBeenWarned=false
+initialised=false
+
+function TIC() --called 60 times per second
+ if initialised==false then
+  initialise()
+ end
+ t=t+1 --master timer
+ cls(0) --screen refresh
+ borderWatch() --keep player in bounds
+ drawPlayer()
+ drawBullet()
+ drawMonster()
+ drawHUD()
+ updateBullet() --move bullets
+ updateMonster() --move beasts
+ cullEntities() --remove bullets that hit edges or monsters, and monsters that get shot or wander off
+ if nextSpawn<=t and math.random(16)==1 then spawnMonster() end
+ sniffControls() --accept input
+ if debug==true then debugHUD() end
+end
+----
+function borderWatch()
+ if player.posit.x<=0 then player.posit.x=1 end
+ if player.posit.x>=233 then player.posit.x=232 end
+ if player.posit.y<=0 then player.posit.y=1 end
+ if player.posit.y>=129 then player.posit.y=128 end
+end
+----
+function sniffControls()
+ if btn(BUTTON_X) then player.aimLock=true else player.aimLock=false end
+ if btnp(BUTTON_A,0,4) and player.ammo>0 then shoot() end
+ if btnp(BUTTON_A,5,60) and player.ammo<1 then sfx(CLICK,'D-3',4,0,15,0) end
+ if btn(PAD_UP) then
+  player.posit.y=player.posit.y-1
+  if not player.aimLock then player.facing=0 end
+ end
+ if btn(PAD_DOWN) then
+  player.posit.y=player.posit.y+1
+  if not player.aimLock then player.facing=1 end
+ end
+ if btn(PAD_LEFT) then
+  player.posit.x=player.posit.x-1
+  if not player.aimLock then player.facing=2 end
+ end
+ if btn(PAD_RIGHT) then
+  player.posit.x=player.posit.x+1
+  if not player.aimLock then player.facing=3 end
+ end
+end
+----
+function shoot()
+ for cb=1, 16 do
+  if bullet[cb].active==true then end
+  if not bullet[cb].active==true then
+   bullet[cb].active=true
+   bullet[cb].outOfBounds=false
+   bullet[cb].velocity=4
+   bullet[cb].facing=player.facing
+   if player.facing==UP then
+    bullet[cb].posit.x=(player.posit.x+MUZZLE_UP_X)
+    bullet[cb].posit.y=(player.posit.y+MUZZLE_UP_Y)
+   end
+   if player.facing==DOWN then
+    bullet[cb].posit.x=(player.posit.x+MUZZLE_DOWN_X)
+    bullet[cb].posit.y=(player.posit.y+MUZZLE_DOWN_Y)
+   end
+   if player.facing==LEFT then
+    bullet[cb].posit.x=(player.posit.x+MUZZLE_LEFT_X)
+    bullet[cb].posit.y=(player.posit.y+MUZZLE_LEFT_Y)
+   end
+   if player.facing==RIGHT then
+    bullet[cb].posit.x=(player.posit.x+MUZZLE_RIGHT_X)
+    bullet[cb].posit.y=(player.posit.y+MUZZLE_RIGHT_Y)
+   end
+   player.ammo=player.ammo-1
+   sfx(GUNFIRE,'C-1',4,0,15,0)
+   break
+  end
+ end
+end
+----
+function drawBullet()
+ for cb=1, 16 do
+  if bullet[cb].facing==UP then
+  line(bullet[cb].posit.x,bullet[cb].posit.y,bullet[cb].posit.x,bullet[cb].posit.y-bullet[cb].length,bullet[cb].colour)
+  end
+  if bullet[cb].facing==DOWN then
+  line(bullet[cb].posit.x,bullet[cb].posit.y,bullet[cb].posit.x,bullet[cb].posit.y+bullet[cb].length,bullet[cb].colour)
+  end
+  if bullet[cb].facing==LEFT then
+  line(bullet[cb].posit.x,bullet[cb].posit.y,bullet[cb].posit.x-bullet[cb].length,bullet[cb].posit.y,bullet[cb].colour)
+  end
+  if bullet[cb].facing==RIGHT then
+  line(bullet[cb].posit.x,bullet[cb].posit.y,bullet[cb].posit.x+bullet[cb].length,bullet[cb].posit.y,bullet[cb].colour)
+  end
+ end
+end
+
+----
+function drawPlayer()
+ if player.facing==UP then 
+  spr(SPR_PLAYER_UP,player.posit.x,player.posit.y,0,1,0,0,1,1)
+ elseif player.facing==DOWN then 
+  spr(SPR_PLAYER_DOWN,player.posit.x,player.posit.y,0,1,0,0,1,1)
+ elseif player.facing==LEFT then 
+  spr(SPR_PLAYER_LEFT,player.posit.x,player.posit.y,0,1,0,0,1,1)
+ elseif player.facing==RIGHT then 
+  spr(SPR_PLAYER_RIGHT,player.posit.x,player.posit.y,0,1,0,0,1,1)
+ end
+end
+----
+function drawMonster()
+  for cm=1, 16 do
+--    spr(SPR_MONSTER,monster[cm].posit.x,monster[cm].posit.y,0,1,0,0,1,1)
+    rect(monster[cm].posit.x,monster[cm].posit.y,8,8,RED)
+  end
+end
+
+function updateBullet()
+ for cb=1, 16 do
+  if bullet[cb].facing==UP and bullet[cb].active==true then
+    bullet[cb].posit.y=bullet[cb].posit.y-bullet[cb].velocity
+  elseif bullet[cb].facing==DOWN and bullet[cb].active==true then
+    bullet[cb].posit.y=bullet[cb].posit.y+bullet[cb].velocity
+  elseif bullet[cb].facing==LEFT and bullet[cb].active==true then
+    bullet[cb].posit.x=bullet[cb].posit.x-bullet[cb].velocity
+  elseif bullet[cb].facing==RIGHT and bullet[cb].active==true then
+    bullet[cb].posit.x=bullet[cb].posit.x+bullet[cb].velocity
+  end
+  if bullet[cb].posit.x>239 then
+    bullet[cb].outOfBounds=true
+  end
+  if bullet[cb].posit.x<0 then
+    bullet[cb].outOfBounds=true
+  end
+  if bullet[cb].posit.y>139 then
+    bullet[cb].outOfBounds=true
+  end
+  if bullet[cb].posit.y<0 then
+    bullet[cb].outOfBounds=true
+  end
+end
+end
+----
+function cullEntities()
+ for cb=1, 16 do
+  if bullet[cb].active==true and bullet[cb].outOfBounds==true then
+   bullet[cb].posit.x=BRAZIL
+   bullet[cb].posit.y=BRAZIL
+   bullet[cb].velocity=0
+   bullet[cb].active=false
+   bullet[cb].outOfBounds=false
+  end
+  if bullet[cb].hit==true then
+    bullet[cb].posit.x=BRAZIL
+    bullet[cb].posit.y=BRAZIL
+    bullet[cb].velocity=0
+    bullet[cb].active=false
+    bullet[cb].outOfBounds=false
+    bullet[cb].hit=false
+   end
+ end
+ for cm=1, 16 do
+  if monster[cm].outOfBounds==true and monster[cm].alive==true then
+   monster[cm].posit.x=(BRAZIL-255)
+   monster[cm].posit.y=(BRAZIL-255)
+   monster[cm].alive=false
+  end
+  if monster[cm].hit==true then
+    monster[cm].posit.x=(BRAZIL-255)
+    monster[cm].posit.y=(BRAZIL-255)
+    monster[cm].alive=false
+    monster[cm].hit=false
+   end
+end
+end
+
+function drawHUD()
+  if player.ammo<200 and not hasBeenWarned==true then
+   sfx(LOW_AMMO,'E-6',70,1)
+   hasBeenWarned=true
+  end
+  if hasBeenWarned==true and player.ammo>250 then
+   hasBeenWarned=false
+  end
+  if player.ammo==0 then
+   print("AMMO: OUT",120,119,ORANGE,false,1,false)
+  elseif player.ammo<200 then
+   print("AMMO: "..player.ammo.."*",120,119,YELLOW,false,1,false)
+  else
+   print("AMMO: "..player.ammo,120,119,LIGHT_GREEN,false,1,false)
+  end
+  print("SCOR: "..score,120,127,LIGHT_GREEN,false,1,false)
+ end
+
+function setupBullets()
+ for cb=1, 16 do --"cb" = "current bullet"
+  bullet[cb]={ --initial bullet stats
+  velocity=0,
+  length=2,
+  colour=YELLOW,
+  facing=UP,
+  active=false,
+  hit=false,
+  posit={
+   x=BRAZIL,
+   y=BRAZIL
+  },
+ }
+ end
+end
+
+function setupMonsters()
+  for cm=1, 16 do --"cm" = "current monster"
+   monster[cm]={ --initial monster stats
+   alive=false,
+   hit=false,
+   speed=0,
+   outOfBounds=true,
+   posit={
+    x=(BRAZIL-255),
+    y=(BRAZIL-255)
+   },
+  }
+  end
+end
+
+function updateMonster()
+ for cm=1, 16 do
+  if monster[cm].posit.x>239 then
+   monster[cm].outOfBounds=true
+  end
+  if monster[cm].posit.x<0 then
+   monster[cm].outOfBounds=true
+  end
+  if monster[cm].posit.y>139 then
+   monster[cm].outOfBounds=true
+  end
+  if monster[cm].posit.y<0 then
+   monster[cm].outOfBounds=true
+  end
+ end
+end
+
+function spawnMonster()
+  for cm=1, 16 do
+   if monster[cm].alive==true then end
+   if monster[cm].alive==false then
+   if monstersSpawned>5 then spawnInterval=300 end
+    monster[cm].posit.x=math.random(8,232)
+    monster[cm].posit.y=math.random(8,128)
+    monster[cm].alive=true
+    monster[cm].outOfBounds=false
+    nextspawn=t+spawnInterval
+    monstersSpawned=monstersSpawned+1
+   break
+   end
+  end
+ end
+
+function randomDrop()
+end
+
+function debugHUD()
+  for ce=1, 16 do
+    print("B"..ce..": "..bullet[ce].posit.x..", "..bullet[ce].posit.y,0,(ce*8),DARK_GREY,true,1,true)
+    print("M"..ce..": "..monster[ce].posit.x..", "..monster[ce].posit.y,80,(ce*8),DARK_GREY,true,1,true)
+  end
+end
+
+
+function initialise()
+ setupBullets()
+ setupMonsters()
+ spawnInterval=4 --try to spawn a monster every 4 tics when starting
+ monstersSpawned=0
+ nextSpawn=t --start spawning immediately
+ initialised=true
+end
+
+function zir()
+end
